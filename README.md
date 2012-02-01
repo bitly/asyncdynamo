e3d139d04a91446352be07b5d8e92a73d06be025
Matt Reiferson <mreiferson@gmail.com>
2012-02-01 21:06:02 +0000

    setup.py packaging

---
+++ README.md
@@ -0,0 +1,6 @@
+asyncdynamo
+-----------
+
+async Amazon DynamoDB library for Tornado
+
+requires boto and python 2.7
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from distutils.core import setup
+
+# also update version in __init__.py
+version = '0.1'
+
+setup(
+    name="asyncdynamo",
+    version=version,
+    keywords=["dynamo", "dynamodb", "amazon", "async", "tornado"],
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md"), "r").read(),
+    description="async Amazon DynamoDB library for Tornado",
+    author="Dan Frank",
+    author_email="df@bit.ly",
+    url="http://github.com/bitly/asyncdynamo",
+    license="Apache Software License",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    packages=['asyncdynamo'],
+    install_requires=['tornado', 'boto'],
+    requires=['tornado'],
+    download_url="http://github.com/downloads/bitly/asyncdynamo/asyncdynamo-%s.tar.gz" % version,
+)
